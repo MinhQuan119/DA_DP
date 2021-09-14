ef54d4f8aac949d0ba2165d25afe84646b8973c7
taile47711 <taile47711@gmail.com>
2021-09-14 17:07:55 +0700

    update tc069

--- TADashboard/src/test/java/tests/general_settings/NegativeGeneralSettingsTest.java
+++ TADashboard/src/test/java/tests/general_settings/NegativeGeneralSettingsTest.java
@@ -1,5 +1,6 @@
 package tests.general_settings;
 
+import com.logigear.driver.DriverUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import page_objects.DashboardPage;
@@ -16,16 +17,16 @@ public class NegativeGeneralSettingsTest extends BaseTest {
     DataProfilesPage dataProfilesPage = new DataProfilesPage();
     DashboardPage dashboardPage = new DashboardPage();
 
-    @Test(description = "Verify that user is unable to proceed to next step or finish creating data profile if  \"Name *\" field is left empty")
+    @Test(description = "Verify that user is unable to proceed to next step or finish creating data profile if  Name field is left empty")
     public void tc069_UserIsUnableToProceedToNextStepOrFinishCreatingDataProfileIfNameFieldIsEmpty() {
         Logger.step("Login with a valid user");
         loginPage.login(Constants.USERNAME, "");
 
         Logger.step("navigate to data profile page");
-        dashboardPage.clickLnkDataProfiles();
+        dashboardPage.clickDataProfilesLink();
 
         Logger.step("Click Add new link");
-        dataProfilesPage.clickLnkAddNew();
+        dataProfilesPage.clickAddNewLink();
 
         Logger.step("click next button");
         generalSettingsPage.clickNextButton();
@@ -34,7 +35,7 @@ public class NegativeGeneralSettingsTest extends BaseTest {
         Assert.assertEquals(generalSettingsPage.getAlertText(), "Please input profile name.", "Dialog message doesn't displayed");
 
         Logger.step("Click Ok in alert");
-        generalSettingsPage.clickOKInAlert();
+        DriverUtils.acceptAlert();
 
         Logger.step("Click finish button");
         generalSettingsPage.clickFinishButton();
