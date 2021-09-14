b5b14148aabb276381e0666de9153072cd1d5fdf
taile47711 <taile47711@gmail.com>
2021-09-14 11:35:29 +0700

    create data profiles page

---
+++ TADashboard/src/main/java/page_objects/DataProfilesPage.java
@@ -0,0 +1,11 @@
+package page_objects;
+
+import com.logigear.control.common.imp.Link;
+
+public class DataProfilesPage {
+    private Link lnkAddNew = new Link("css=div[class='panel_tag2'] a[href='profile.jsp?action=create']");
+
+    public void clickLnkAddNew(){
+        lnkAddNew.click();
+    }
+}
