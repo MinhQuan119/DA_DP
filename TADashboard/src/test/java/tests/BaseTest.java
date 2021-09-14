1f627f7db6f72f509c7440cc27a9f986b2ff05a7
taile47711 <taile47711@gmail.com>
2021-09-14 11:34:50 +0700

    update base test

--- TADashboard/src/test/java/tests/BaseTest.java
+++ TADashboard/src/test/java/tests/BaseTest.java
@@ -5,15 +5,15 @@ import com.logigear.driver.DriverUtils;
 import com.logigear.helper.BrowserSettingHelper;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import utils.common.Constants;
-import utils.helper.Logger;
 
 public class BaseTest {
 
 	@BeforeMethod
 	@Parameters("Browser")
-	public void beforeMethod(String browser) throws Exception {
+	public void beforeMethod(@Optional("chrome.local") String browser) throws Exception {
 		DriverProperty driverProperty = BrowserSettingHelper.getDriverProperty(Constants.BROWSER_SETTING_FILE, browser);
 		DriverUtils.getDriver(driverProperty);
 		DriverUtils.setTimeOut(Constants.TIME_OUT);
