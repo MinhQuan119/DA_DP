695327c588e1359743090fa94e1f3b0233fee14d
taile47711 <taile47711@gmail.com>
2021-09-14 10:46:26 +0700

    update dashboard page

--- TADashboard/src/main/java/page_objects/DashboardPage.java
+++ TADashboard/src/main/java/page_objects/DashboardPage.java
@@ -1,11 +1,19 @@
 package page_objects;
 
 import com.logigear.control.common.imp.Label;
+import com.logigear.control.common.imp.Link;
 
 public class DashboardPage {
     private Label lblWelcome = new Label("css=a[href='#Welcome']");
+    private Link lnkDataProfiles = new Link("css=li>ul>li>a[href='profiles.jsp']");
+    private Link lnkAdminister = new Link("css=a[href='#Administer']");
 
     public boolean doesWelcomeLabelDisplay() {
         return lblWelcome.isVisible();
+    }
+
+    public void clickLnkDataProfiles(){
+        lnkAdminister.click();
+        lnkDataProfiles.click();
     }
 }
