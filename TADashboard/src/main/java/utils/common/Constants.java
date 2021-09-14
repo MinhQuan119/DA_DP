5a3f47ed5eefc3139c7edfa272a42fddc8641ecc
taile47711 <taile47711@gmail.com>
2021-09-14 10:49:54 +0700

    update

--- TADashboard/src/main/java/utils/common/Constants.java
+++ TADashboard/src/main/java/utils/common/Constants.java
@@ -4,13 +4,13 @@ import utils.helper.PropertiesHelper;
 
 public class Constants {
 
-	// Common Constants
-	public static final String BROWSER_SETTING_FILE = "src/test/resources/browsers.setting.ini";
-	public static final int TIME_OUT = Integer
-			.parseInt(PropertiesHelper.getPropValue("driver.timeout"));
-	public static final int SHORT_TIME_OUT = Integer
-			.parseInt(PropertiesHelper.getPropValue("driver.shortTimeout"));
+    // Common Constants
+    public static final String BROWSER_SETTING_FILE = "src/test/resources/browsers.setting.ini";
+    public static final int TIME_OUT = Integer
+            .parseInt(PropertiesHelper.getPropValue("driver.timeout"));
+    public static final int SHORT_TIME_OUT = Integer
+            .parseInt(PropertiesHelper.getPropValue("driver.shortTimeout"));
 
-	// AUT Constants
-	public static final String AUT_URL = PropertiesHelper.getPropValue("aut.url");
+    // AUT Constants
+    public static final String AUT_URL = PropertiesHelper.getPropValue("aut.url");
 }
--- TADashboard/src/test/java/tests/BaseTest.java
+++ TADashboard/src/test/java/tests/BaseTest.java
@@ -7,7 +7,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import utils.common.Constants;
-import utils.helper.Logger;
 
 public class BaseTest {
 
