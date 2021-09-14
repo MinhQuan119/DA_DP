3bf38059adfdd1dbcc5fdd728f87e3d7276d9123
ltthai07t3 <ltthai07t3@gmail.com>
2021-09-15 00:27:03 +0700

    fix for comment

--- TADashboard/src/main/java/page_objects/DataProfilesPage.java
+++ TADashboard/src/main/java/page_objects/DataProfilesPage.java
@@ -5,22 +5,22 @@ import com.logigear.control.common.imp.Link;
 import utils.common.Constants;
 
 public class DataProfilesPage {
-    private CheckBox dynamicCheckColumn = new CheckBox("xpath=//table[@class='GridView']/tbody/tr/td[text()='%s']/preceding-sibling::td/input");
+    private CheckBox dynamicCheckbox = new CheckBox("xpath=//table[@class='GridView']/tbody/tr/td[text()='%s']/preceding-sibling::td/input");
     private Link dynamicDataProfile = new Link("xpath=//table[@class='GridView']/tbody/tr/td[text()='%s']");
     private Link dynamicAction = new Link("xpath=//table[@class='GridView']/tbody/tr/td[text()='%s']/following::td[@class='center']/a/parent::td");
 
     public String getTextOfAction(String string) {
-        dynamicAction.setDynamicValue(string);
+        dynamicAction.setDynamicValue(string.replace(" ", " "));
         return dynamicAction.getText();
     }
 
     public void clickOnDataProfile(String string) {
-        dynamicDataProfile.setDynamicValue(string);
+        dynamicDataProfile.setDynamicValue(string.replace(" ", " "));
         dynamicDataProfile.click();
     }
 
     public boolean isCheckBoxDisplayed(String string) {
-        dynamicCheckColumn.setDynamicValue(string);
-        return dynamicCheckColumn.isVisible(Constants.SHORT_TIME_OUT);
+        dynamicCheckbox.setDynamicValue(string.replace(" ", " "));
+        return dynamicCheckbox.isVisible(Constants.SHORT_TIME_OUT);
     }
 }
