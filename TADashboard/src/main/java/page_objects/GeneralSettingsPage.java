c4215b7f00e42b02da395883100f882d2104135c
taile47711 <taile47711@gmail.com>
2021-09-15 09:34:33 +0700

    update general settings page

--- TADashboard/src/main/java/page_objects/GeneralSettingsPage.java
+++ TADashboard/src/main/java/page_objects/GeneralSettingsPage.java
@@ -1,11 +1,14 @@
 package page_objects;
 
 import com.logigear.control.common.imp.Button;
+import com.logigear.control.common.imp.Link;
 import com.logigear.driver.DriverUtils;
 
 public class GeneralSettingsPage {
     private Button btnNext = new Button("css=input[value='Next']");
     private Button btnFinish = new Button("css=input[value='Finish']");
+    private Link lnkDataProfiles = new Link("css=li>ul>li>a[href='profiles.jsp']");
+    private Link lnkAdminister = new Link("css=a[href='#Administer']");
 
     public void clickNextButton() {
         btnNext.click();
@@ -15,7 +18,8 @@ public class GeneralSettingsPage {
         btnFinish.click();
     }
 
-    public String getAlertText() {
-        return DriverUtils.getAlertText();
+    public void selectAdministerMenu(DataProfilesPage dataProfilesPage) {
+        lnkAdminister.click();
+        lnkDataProfiles.click();
     }
 }
