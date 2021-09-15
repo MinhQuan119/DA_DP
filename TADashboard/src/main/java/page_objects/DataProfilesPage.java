9897d611c86b7de11bf10ba1ac8ab438dde08b0a
taile47711 <taile47711@gmail.com>
2021-09-15 16:00:48 +0700

    update data profiles page

--- TADashboard/src/main/java/page_objects/DataProfilesPage.java
+++ TADashboard/src/main/java/page_objects/DataProfilesPage.java
@@ -2,18 +2,10 @@ package page_objects;
 
 import com.logigear.control.common.imp.Link;
 
-public class DataProfilesPage {
+public class DataProfilesPage extends BasePage{
     private Link lnkAddNew = new Link("css=div[class='panel_tag2'] a[href='profile.jsp?action=create']");
-    private Link dynamicNavigateToPage = new Link("xpath=//ul[@id='ulAdminister']/li/a[text()='%s']");
-    private Link lnkAdminister = new Link("css=a[href='#Administer']");
 
     public void clickAddNewLink(){
         lnkAddNew.click();
-    }
-
-    public void selectInAdministerMenu(String pageName) {
-        lnkAdminister.click();
-        dynamicNavigateToPage.setDynamicValue(pageName);
-        dynamicNavigateToPage.click();
     }
 }
