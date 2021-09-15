78bd904a99ae1c76cc209b6e6bf8afa54b735bcd
taile47711 <taile47711@gmail.com>
2021-09-15 09:42:22 +0700

    update data profiles page

--- TADashboard/src/main/java/page_objects/DataProfilesPage.java
+++ TADashboard/src/main/java/page_objects/DataProfilesPage.java
@@ -4,8 +4,14 @@ import com.logigear.control.common.imp.Link;
 
 public class DataProfilesPage {
     private Link lnkAddNew = new Link("css=div[class='panel_tag2'] a[href='profile.jsp?action=create']");
+    private Link lnkDataProfiles = new Link("css=li>ul>li>a[href='profiles.jsp']");
+    private Link lnkAdminister = new Link("css=a[href='#Administer']");
 
     public void clickAddNewLink(){
         lnkAddNew.click();
+    }
+    public void selectAdministerMenu(String string) {
+        lnkAdminister.click();
+        lnkDataProfiles.click();
     }
 }
