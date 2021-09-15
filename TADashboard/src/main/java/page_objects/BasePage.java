989ac7720d290c09f1e6dacaec9b0e2070bc0c1c
taile47711 <taile47711@gmail.com>
2021-09-15 16:01:12 +0700

    create base page

--- TADashboard/src/main/java/page_objects/BasePage.java
+++ TADashboard/src/main/java/page_objects/BasePage.java
@@ -1,2 +1,14 @@
-package page_objects;public class BasePage {
+package page_objects;
+
+import com.logigear.control.common.imp.Link;
+
+public class BasePage {
+    private Link dynamicNavigateToPage = new Link("xpath=//ul[@id='ulAdminister']/li/a[text()='%s']");
+    private Link lnkAdminister = new Link("css=a[href='#Administer']");
+
+    public void selectAdministerMenu(String pageName) {
+        lnkAdminister.click();
+        dynamicNavigateToPage.setDynamicValue(pageName);
+        dynamicNavigateToPage.click();
+    }
 }
