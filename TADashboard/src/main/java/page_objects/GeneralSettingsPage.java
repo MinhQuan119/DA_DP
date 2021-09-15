51f421a936d2a99e5b7a63f36a854a9afe14eefd
taile47711 <taile47711@gmail.com>
2021-09-15 09:42:55 +0700

    general settings page

--- TADashboard/src/main/java/page_objects/GeneralSettingsPage.java
+++ TADashboard/src/main/java/page_objects/GeneralSettingsPage.java
@@ -7,8 +7,6 @@ import com.logigear.driver.DriverUtils;
 public class GeneralSettingsPage {
     private Button btnNext = new Button("css=input[value='Next']");
     private Button btnFinish = new Button("css=input[value='Finish']");
-    private Link lnkDataProfiles = new Link("css=li>ul>li>a[href='profiles.jsp']");
-    private Link lnkAdminister = new Link("css=a[href='#Administer']");
 
     public void clickNextButton() {
         btnNext.click();
@@ -16,10 +14,5 @@ public class GeneralSettingsPage {
 
     public void clickFinishButton() {
         btnFinish.click();
-    }
-
-    public void selectAdministerMenu(DataProfilesPage dataProfilesPage) {
-        lnkAdminister.click();
-        lnkDataProfiles.click();
     }
 }
