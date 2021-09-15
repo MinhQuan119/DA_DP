42b8d550c1923bc96a3d666a3c02ff0e0238d179
taile47711 <taile47711@gmail.com>
2021-09-15 16:36:54 +0700

    update base page

--- TADashboard/src/main/java/page_objects/BasePage.java
+++ TADashboard/src/main/java/page_objects/BasePage.java
@@ -3,12 +3,12 @@ package page_objects;
 import com.logigear.control.common.imp.Link;
 
 public class BasePage {
-    private Link dynamicNavigateToPage = new Link("xpath=//ul[@id='ulAdminister']/li/a[text()='%s']");
+    private Link dynamicAdministerMenu = new Link("xpath=//ul[@id='ulAdminister']/li/a[text()='%s']");
     private Link lnkAdminister = new Link("css=a[href='#Administer']");
 
     public void selectAdministerMenu(String pageName) {
         lnkAdminister.click();
-        dynamicNavigateToPage.setDynamicValue(pageName);
-        dynamicNavigateToPage.click();
+        dynamicAdministerMenu.setDynamicValue(pageName);
+        dynamicAdministerMenu.click();
     }
 }
