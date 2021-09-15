ca1ae0765a7e9912acda97692ea7adaa7ed18a55
taile47711 <taile47711@gmail.com>
2021-09-15 09:41:56 +0700

    update tc069

--- TADashboard/src/test/java/tests/general_settings/NegativeGeneralSettingsTest.java
+++ TADashboard/src/test/java/tests/general_settings/NegativeGeneralSettingsTest.java
@@ -23,7 +23,7 @@ public class NegativeGeneralSettingsTest extends BaseTest {
         loginPage.login(Constants.USERNAME, "");
 
         Logger.step("navigate to data profile page");
-        generalSettingsPage.selectAdministerMenu(dataProfilesPage);
+        dataProfilesPage.selectAdministerMenu("data profiles");
 
         Logger.step("Click Add new link");
         dataProfilesPage.clickAddNewLink();
