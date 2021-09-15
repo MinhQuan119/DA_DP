2036596d5d03b728a5a7d17bab4d80ee9c668a2e
ltthai07t3 <ltthai07t3@gmail.com>
2021-09-15 17:28:58 +0700

    Update xpath

--- TADashboard/src/main/java/page_objects/DataProfilesPage.java
+++ TADashboard/src/main/java/page_objects/DataProfilesPage.java
@@ -5,22 +5,24 @@ import com.logigear.control.common.imp.Link;
 import utils.common.Constants;
 
 public class DataProfilesPage extends BasePage {
-    private CheckBox dynamicCheckbox = new CheckBox("xpath=//table[@class='GridView']/tbody/tr/td[text()='%s']/preceding-sibling::td/input");
-    private Link dynamicDataProfile = new Link("xpath=//table[@class='GridView']/tbody/tr/td[text()='%s']");
-    private Link dynamicAction = new Link("xpath=//table[@class='GridView']/tbody/tr/td[text()='%s']/following::td[@class='center']/a/parent::td");
+    private CheckBox dynamicPresetCheckbox = new CheckBox("xpath=//table[@class='GridView']/tbody/tr/td[text()='%s']/preceding-sibling::td/input");
+    private CheckBox dynamicNonPresetCheckbox = new CheckBox("xpath=//table[@class='GridView']/tbody/tr/td/a[text()='%s']/parent::td/preceding-sibling::td/input");
+    private Link dynamicPresetDataProfile = new Link("xpath=//table[@class='GridView']/tbody/tr/td[text()='%s']");
+    private Link dynamicNonPresetDataProfile = new Link("xpath=//table[@class='GridView']/tbody/tr/td/a[text()='%s']");
+    private Link dynamicAction = new Link("xpath=//table[@class='GridView']/tbody/tr/td[text()='%s']/following-sibling::td[@class='center']/a/parent::td");
 
     public String getTextOfAction(String action) {
         dynamicAction.setDynamicValue(action.replace(" ", " "));
         return dynamicAction.getText();
     }
 
-    public void clickOnDataProfile(String dataProfile) {
-        dynamicDataProfile.setDynamicValue(dataProfile.replace(" ", " "));
-        dynamicDataProfile.click();
+    public void clickOnPresetDataProfile(String dataProfile) {
+        dynamicPresetDataProfile.setDynamicValue(dataProfile.replace(" ", " "));
+        dynamicPresetDataProfile.click();
     }
 
-    public boolean isCheckBoxDisplayed(String checkbox) {
-        dynamicCheckbox.setDynamicValue(checkbox.replace(" ", " "));
-        return dynamicCheckbox.isVisible(Constants.SHORT_TIME_OUT);
+    public boolean isPresetCheckBoxDisplayed(String checkbox) {
+        dynamicPresetCheckbox.setDynamicValue(checkbox.replace(" ", " "));
+        return dynamicPresetCheckbox.isVisible(Constants.SHORT_TIME_OUT);
     }
 }
