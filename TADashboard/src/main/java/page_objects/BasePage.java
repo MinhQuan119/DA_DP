3e23706e3d9e3574395c4739d010c0ac2f9193b7
taile47711 <taile47711@gmail.com>
2021-09-16 16:43:24 +0700

    change variable from pageName to menu

--- TADashboard/src/main/java/page_objects/BasePage.java
+++ TADashboard/src/main/java/page_objects/BasePage.java
@@ -6,9 +6,9 @@ public class BasePage {
     private Link dynamicAdministerMenu = new Link("xpath=//ul[@id='ulAdminister']/li/a[text()='%s']");
     private Link lnkAdminister = new Link("css=a[href='#Administer']");
 
-    public void selectAdministerMenu(String pageName) {
+    public void selectAdministerMenu(String menu) {
         lnkAdminister.click();
-        dynamicAdministerMenu.setDynamicValue(pageName);
+        dynamicAdministerMenu.setDynamicValue(menu);
         dynamicAdministerMenu.click();
     }
 }
