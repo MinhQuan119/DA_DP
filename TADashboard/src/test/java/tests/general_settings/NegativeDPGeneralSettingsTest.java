90c85734b9285e31b369caef3a09ecc7b355e9b4
taile47711 <taile47711@gmail.com>
2021-09-20 15:46:00 +0700

    update tc073

--- TADashboard/src/test/java/tests/general_settings/NegativeDPGeneralSettingsTest.java
+++ TADashboard/src/test/java/tests/general_settings/NegativeDPGeneralSettingsTest.java
@@ -2,7 +2,9 @@ package tests.general_settings;
 
 import com.logigear.driver.DriverUtils;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import page_objects.DashboardPage;
 import page_objects.DataProfilesPage;
 import page_objects.DataProfilesGeneralSettingsPage;
 import page_objects.LoginPage;
@@ -10,22 +12,29 @@ import tests.BaseTest;
 import utils.common.Constants;
 import utils.helper.Logger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NegativeDPGeneralSettingsTest extends BaseTest {
     DataProfilesGeneralSettingsPage dataProfilesGeneralSettingsPage = new DataProfilesGeneralSettingsPage();
     LoginPage loginPage = new LoginPage();
     DataProfilesPage dataProfilesPage = new DataProfilesPage();
+    DashboardPage dashboardPage = new DashboardPage();
 
-    @Test(description = "Verify that user is unable to proceed to next step or finish creating data profile if 'Name*' field is left empty")
-    public void tc069_UserIsUnableToProceedToNextStepOrFinishCreatingDataProfileIfNameFieldIsEmpty() {
+    @BeforeMethod
+    public void loginAndNavigateToDPPage() {
         Logger.step("Login with a valid user");
         loginPage.login(Constants.USERNAME, "");
 
-        Logger.step("Navigate to data profile page");
-        dataProfilesPage.selectAdministerMenu("Data Profiles");
+        Logger.step("Go to Data Profiles page.");
+        dashboardPage.selectAdministerMenu("Data Profiles");
 
         Logger.step("Click Add New link");
         dataProfilesPage.clickAddNewLink();
+    }
 
+    @Test(description = "Verify that user is unable to proceed to next step or finish creating data profile if 'Name*' field is left empty")
+    public void tc069_UserIsUnableToProceedToNextStepOrFinishCreatingDataProfileIfNameFieldIsEmpty() {
         Logger.step("Click Next button");
         dataProfilesGeneralSettingsPage.clickNextButton();
 
@@ -40,5 +49,26 @@ public class NegativeDPGeneralSettingsTest extends BaseTest {
 
         Logger.verify("Verify that dialog message is displayed");
         Assert.assertEquals(DriverUtils.getAlertText(), "Please input profile name.", "Dialog doesn't display correct message");
+    }
+
+    @Test(description = "Verify that all data profile types are listed in priority order under 'Item Type' dropped down menu")
+    public void tc073_AllDataProfileTypesAreListed() {
+        List<String> optionInTypeItem = new ArrayList<>();
+        optionInTypeItem.add("test modules");
+        optionInTypeItem.add("test cases");
+        optionInTypeItem.add("test objectives");
+        optionInTypeItem.add("data sets");
+        optionInTypeItem.add("actions");
+        optionInTypeItem.add("interface entities");
+        optionInTypeItem.add("test results");
+        optionInTypeItem.add("test case results");
+        optionInTypeItem.add("test suites");
+        optionInTypeItem.add("bugs");
+        Logger.verify("Items are listed in priority order");
+        dataProfilesGeneralSettingsPage.clickItemTypeComboBox();
+        for (String option : optionInTypeItem) {
+            Assert.assertTrue(dataProfilesGeneralSettingsPage.isOptionVisible(option),
+                    "items are unlisted in priority order");
+        }
     }
 }
