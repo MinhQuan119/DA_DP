caeefb10598ada0bd73e1d3ae86b8026de35af2e
taile47711 <taile47711@gmail.com>
2021-09-23 10:19:00 +0700

    update data profiles display fields

--- TADashboard/src/main/java/page_objects/DataProfilesDisplayFieldsPage.java
+++ TADashboard/src/main/java/page_objects/DataProfilesDisplayFieldsPage.java
@@ -1,14 +1,14 @@
 package page_objects;
 
-import com.logigear.control.common.imp.CheckBox;
+import com.logigear.control.common.imp.Label;
 
 import java.util.List;
 
 public class DataProfilesDisplayFieldsPage {
-    private CheckBox dynamicCheckBox = new CheckBox("css=table[id='profilesettings'] tbody label");
+    private Label lblDisplayField = new Label("css=table[id='profilesettings'] tbody label");
 
     public List<String> getTextOfCheckBox() {
-        dynamicCheckBox.waitForDisplay();
-        return dynamicCheckBox.getListText();
+        lblDisplayField.waitForDisplay();
+        return lblDisplayField.getListText();
     }
 }
